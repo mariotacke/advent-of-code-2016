979596c835f666dfca4dec973d8899b0e4386301
Mario Tacke <tacke.mario@gmail.com>
2017-12-02 12:39:55 -0800

    feature: No Time for a Taxicab (part 2)

---
+++ day-01-no-time-for-a-taxicab/no-time-for-a-taxicab2.js
@@ -0,0 +1,94 @@
+const noTimeForATaxicab = (input) => {
+  let output;
+  
+  const pastLocations = {};
+  
+  const walkPoints = (p1, p2) => {
+    const xLength = p2.x - p1.x;
+    const yLength = p2.y - p1.y;
+    
+    if (p1.y === p2.y) {
+      for (let t = 1; t <= Math.abs(xLength); t++) {
+        const x = p1.x + t * Math.sign(xLength);
+        
+        if (!pastLocations[`${x}:${p1.y}`]) {
+          pastLocations[`${x}:${p1.y}`] = 1;
+        }
+        
+        if (pastLocations[`${x}:${p1.y}`] === 2) {
+          return { x, y: p1.y };
+        } else {
+          pastLocations[`${x}:${p1.y}`] += 1;
+        }
+      }
+    }
+    
+    if (p1.x === p2.x) {
+      for (let t = 1; t <= Math.abs(yLength); t++) {
+        const y = p1.y + t * Math.sign(yLength);
+
+        if (!pastLocations[`${p1.x}:${y}`]) {
+          pastLocations[`${p1.x}:${y}`] = 1;
+        }
+
+        if (pastLocations[`${p1.x}:${y}`] === 2) {
+          return { x: p1.x, y };
+        } else {
+          pastLocations[`${p1.x}:${y}`] += 1;
+        }
+      }
+    }
+  };
+  
+  const reduced = input
+    .split(', ')
+    .map((x) => {
+      return {
+        direction: x[0],
+        steps: parseInt(x.substring(1, x.length)),
+      };
+    })
+    .reduce((accumulator, currentValue) => {  
+      let heading = accumulator.heading;
+      let x = 0;
+      let y = 0;
+      
+      switch (heading) {
+        case 'N':
+          heading = currentValue.direction === 'L' ? 'W' : 'E';
+          x += currentValue.steps * (heading === 'W' ? -1 : 1);
+          break;
+        case 'E':
+          heading = currentValue.direction === 'L' ? 'N' : 'S';
+          y += currentValue.steps * (heading === 'S' ? -1 : 1);
+          break;
+        case 'S':
+          heading = currentValue.direction === 'L' ? 'E' : 'W';
+          x += currentValue.steps * (heading === 'W' ? -1 : 1);
+          break;
+        case 'W':
+          heading = currentValue.direction === 'L' ? 'S' : 'N';
+          y += currentValue.steps * (heading === 'S' ? -1 : 1);
+          break;
+      }
+
+      const start = { x: accumulator.x, y: accumulator.y };
+      const end = { x: accumulator.x + x, y: accumulator.y + y };
+
+      const cross = walkPoints(start, end);
+
+      if (cross && !output) {
+        output = { x: cross.x, y: cross.y };
+      }
+
+      accumulator.heading = heading;
+      accumulator.x += x;
+      accumulator.y += y;
+
+      return accumulator;
+    }, { heading: 'N', x: 0, y: 0 });
+    
+    return Math.abs(output.x) + Math.abs(output.y);;
+};
+
+module.exports = noTimeForATaxicab;
--- day-01-no-time-for-a-taxicab/test.js
+++ day-01-no-time-for-a-taxicab/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const noTimeForATaxicab = require('./no-time-for-a-taxicab');
+const noTimeForATaxicab2 = require('./no-time-for-a-taxicab2');
 
 describe('Day 1: No Time for a Taxicab', () => {
   it('should properly calculate R2, L3', () => {
@@ -13,5 +14,11 @@ describe('Day 1: No Time for a Taxicab', () => {
   
   it('should properly calculate R5, L5, R5, R3', () => {    
     assert.equal(12, noTimeForATaxicab('R5, L5, R5, R3'));
+  });
+  
+  describe('Part 2', () => {
+    it('should properly calculate R8, R4, R4, R8', () => {    
+      assert.equal(4, noTimeForATaxicab2('R8, R4, R4, R8'));
+    });
   });
 });
