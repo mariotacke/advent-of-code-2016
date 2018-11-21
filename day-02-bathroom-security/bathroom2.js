60dda58feb1246decf8987598c8f93ea4486f498
Mario Tacke <tacke.mario@gmail.com>
2018-11-20 20:47:11 -0800

    feature: Bathroom Security (part 2)

---
+++ day-02-bathroom-security/bathroom2.js
@@ -0,0 +1,45 @@
+class Keypad {
+  constructor () {
+    this._pad = [
+      ['',  '',  '1', '',  ''],
+      ['',  '2', '3', '4', ''],
+      ['5', '6', '7', '8', '9'],
+      ['',  'A', 'B', 'C', ''],
+      ['',  '',  'D', '',  ''],
+    ];
+
+    this._x = 0;
+    this._y = 2;
+  }
+
+  get number () {
+    return this._pad[this._y][this._x];
+  }
+
+  move (direction) {
+    const x = direction === 'L' ? -1 : direction === 'R' ? 1 : 0;
+    const y = direction === 'U' ? -1 : direction === 'D' ? 1 : 0;
+
+    this._x = (this._pad[this._y] || [])[this._x + x] ? this._x + x : this._x;
+    this._y = (this._pad[this._y + y] || [])[this._x] ? this._y + y : this._y;
+  }
+}
+
+const bathroom = (input) => {
+  const keypad = new Keypad();
+
+  const sequence = input
+    .split('\n')
+    .map((line) => {
+      line
+        .trim()
+        .split('')
+        .forEach((direction) => keypad.move(direction))
+
+      return keypad.number;
+    });
+
+  return sequence.join('');
+};
+
+module.exports = bathroom;
--- day-02-bathroom-security/test.js
+++ day-02-bathroom-security/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const bathroom = require('./bathroom');
+const bathroom2 = require('./bathroom2');
 
 describe('Day 2: Bathroom Security', () => {
   it('should properly calculate bathroom code', () => {
@@ -11,5 +12,17 @@ describe('Day 2: Bathroom Security', () => {
        UUUUD`;
 
     assert.strictEqual(bathroom(sequence), '1985');
+  });
+
+  describe('Part Two', () => {
+    it('should properly calculate real bathroom code', () => {
+      const sequence =
+        `ULL
+         RRDDD
+         LURDL
+         UUUUD`;
+
+      assert.strictEqual(bathroom2(sequence), '5DB3');
+    });
   });
 });
