e801a34a60ed903746afdad66c2be8e92bb7a2a3
Mario Tacke <tacke.mario@gmail.com>
2018-11-22 09:57:49 -0800

    feature: Signals and Noise (part 2)

--- day-06-signals-and-noise/test.js
+++ day-06-signals-and-noise/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const denoise = require('./denoise');
+const denoise2 = require('./denoise2');
 
 describe('Day 6: Signals and Noise', () => {
   it('should denoise message', () => {
@@ -23,5 +24,29 @@ describe('Day 6: Signals and Noise', () => {
        enarar`;
 
     assert.strictEqual(denoise(signals), 'easter');
+  });
+
+  describe('Part Two', () => {
+    it('should properly denoise message', () => {
+      const signals =
+        `eedadn
+         drvtee
+         eandsr
+         raavrd
+         atevrs
+         tsrnev
+         sdttsa
+         rasrtv
+         nssdts
+         ntnada
+         svetve
+         tesnvt
+         vntsnd
+         vrdear
+         dvrsen
+         enarar`;
+
+      assert.strictEqual(denoise2(signals), 'advent');
+    });
   });
 });
