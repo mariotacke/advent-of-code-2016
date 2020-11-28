fe11c89c83245fdd0f96b53a9741e9d46da35fb6
Mario Tacke <tacke.mario@gmail.com>
2020-11-28 14:29:43 -0800

    feat: Two Steps Forward (part 2)

--- day-17-two-steps-forward/test.js
+++ day-17-two-steps-forward/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const vault = require('./vault');
+const vault2 = require('./vault2');
 
 describe('Day 17: Two Steps Forward', () => {
   it('should find shortest path to vault (ihgpwlah)', () => {
@@ -19,5 +20,25 @@ describe('Day 17: Two Steps Forward', () => {
     const seed = 'ulqzkmiv';
 
     assert.strictEqual(vault(seed), 'DRURDRUDDLLDLUURRDULRLDUUDDDRR');
+  });
+
+  describe('Part Two', () => {
+    it('should find longest path to vault (ihgpwlah)', () => {
+      const seed = 'ihgpwlah';
+
+      assert.strictEqual(vault2(seed), 370);
+    });
+
+    it('should find longest path to vault (kglvqrro)', () => {
+      const seed = 'kglvqrro';
+
+      assert.strictEqual(vault2(seed), 492);
+    });
+
+    it('should find longest path to vault (ulqzkmiv)', () => {
+      const seed = 'ulqzkmiv';
+
+      assert.strictEqual(vault2(seed), 830);
+    });
   });
 });
---
+++ day-17-two-steps-forward/vault2.js
@@ -0,0 +1,75 @@
+const crypto = require('crypto');
+
+const md5 = (value) => crypto.createHash('md5').update(value).digest('hex');
+
+function getMoves(hash) {
+  return hash
+    .slice(0, 4)
+    .split('')
+    .map((direction) => /[bcdef]/.test(direction));
+}
+
+const vectors = {
+  U: [0, -1],
+  D: [0, 1],
+  L: [-1, 0],
+  R: [1, 0],
+};
+
+function move(sequence) {
+  return sequence.reduce(([px, py], instruction) => {
+    const [dx, dy] = vectors[instruction];
+
+    return [px + dx, py + dy];
+  }, [0, 0]);
+}
+
+function withinField([px, py], [maxX, maxY], direction) {
+  const [dx, dy] = vectors[direction];
+
+  return px + dx >= 0 && px + dx <= maxX && py + dy >= 0 && py + dy <= maxY;
+}
+
+function search(seed, to) {
+  const [, D, , R] = getMoves(md5(seed));
+  let paths = [];
+  let nextPaths = [];
+
+  // U and L impossible at start
+  if (D) { paths.push(['D']); }
+  if (R) { paths.push(['R']); }
+
+  let longestPath = 0;
+
+  while (paths.length) {
+    nextPaths = [];
+
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+      const [px, py] = move(path);
+
+      if (px === to[0] && py === to[1]) {
+        if (path.length > longestPath) {
+          longestPath = path.length;
+        }
+
+        continue;
+      }
+
+      const [U, D, L, R] = getMoves(md5(`${seed}${path.join('')}`));
+
+      U && withinField([px, py], to, 'U') && nextPaths.push([...path, 'U']);
+      D && withinField([px, py], to, 'D') && nextPaths.push([...path, 'D']);
+      L && withinField([px, py], to, 'L') && nextPaths.push([...path, 'L']);
+      R && withinField([px, py], to, 'R') && nextPaths.push([...path, 'R']);
+    }
+
+    paths = nextPaths;
+  }
+
+  return longestPath;
+}
+
+module.exports = (seed, target = [3, 3]) => {
+  return search(seed, target);
+};
