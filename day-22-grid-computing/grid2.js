b2726c7ccb686bf2b1167ab4724006d99be6314f
Mario Tacke <tacke.mario@gmail.com>
2021-11-24 15:17:35 -0800

    wip

---
+++ day-22-grid-computing/grid2.js
@@ -0,0 +1,38 @@
+module.exports = (input) => {
+  const df = input
+    .split('\n')
+    .slice(2)
+    .map((line) => line
+      .match(/x(\d+)-y(\d+)\s+\d+T\s+(\d+)T\s+(\d+)/)
+      .slice(1)
+      .map((x) => parseInt(x))
+    );
+
+  const highestX = df
+    .filter(([, y]) => y === 0)
+    .sort((a, b) => b[0] - a[0])[0][0];
+
+  console.log(highestX);
+
+  let viablePairs = 0;
+
+  for (let a = 0; a < df.length; a++) {
+    const nodeA = df[a];
+
+    if (nodeA[2] === 0) {
+      continue;
+    }
+
+    const otherNodes = df.filter(([x, y]) => x !== nodeA.x && y !== nodeA.y);
+
+    for (let b = 0; b < otherNodes.length; b++) {
+      const nodeB = otherNodes[b];
+
+      if (nodeB[3] >= nodeA[2]) {
+        viablePairs++;
+      }
+    }
+  }
+
+  return viablePairs;
+};
